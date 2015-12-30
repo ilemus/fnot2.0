464707c5b6cd227549d2d1ce69a6866def9e3e71
Isaac Lemus <ilemus@mobis-usa.com>
2015-12-30 15:16:50 -0500

    adds developer option, not entirely user resistant though

--- app/src/main/java/com/home/ilemus/fnot/MainActivity.java
+++ app/src/main/java/com/home/ilemus/fnot/MainActivity.java
@@ -1,6 +1,8 @@
 package com.home.ilemus.fnot;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,36 +14,63 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_STRING = "com.home.ilemus.fnot.PERSONNAME";
     public final static String EXTRA_PASS = "come.home.ilemus.fnot.PASSWORD";
+    public static boolean isDeveloper = false;
+    private static int keyPresses;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        keyPresses = 0;
+        final Context context = this;
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                keyPresses++;
+                if (keyPresses > 0) {
+                    if (keyPresses > 5) {
+                        isDeveloper = true;
+                        Intent intent = new Intent(context, HomeActivity.class);
+                        intent.putExtra(EXTRA_STRING, "Developer");
+                        intent.putExtra(EXTRA_PASS, "Admin");
+                        startActivity(intent);
+                    }
+                    //TODO make it so that a user is timed
+                    //Chayim5756, Chayim5756, thisKey
+                }
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void buttonPressed(View view){
-        EditText name = (EditText)findViewById(R.id.PersonName);
+    public void buttonPressed(View view) {
+        EditText name = (EditText) findViewById(R.id.PersonName);
         String person = name.getText().toString();
-        name = (EditText)findViewById(R.id.Password);
+        name = (EditText) findViewById(R.id.Password);
         String password = name.getText().toString();
         Intent intent = new Intent(this, HomeActivity.class);
         intent.putExtra(EXTRA_STRING, person);
         intent.putExtra(EXTRA_PASS, password);
         startActivity(intent);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -62,5 +91,45 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.home.ilemus.fnot/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.home.ilemus.fnot/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
